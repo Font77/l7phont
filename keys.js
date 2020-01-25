5c08756a599f0c5ec18e493755e5aaac783d61c4
vimal kumar <viml.vnindia@gmail.com>
2020-01-25 12:02:04 +0530

    chenjd lato
    chenjd lato

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // mongodbURI: '<mongodbURI>'
+    //mongodbURI: 'mongodb://ashton:ashton1@ds139242.mlab.com:39242/shopping_cart'
+    mongodbURI: 'mongodb+srv://viml:pasvord828@heksadesimlhindi-wb2ge.gcp.mongodb.net/test?retryWrites=true&w=majority'
+}
+
