b407a14f690571ebb1bb35c057922679e74adc2b
Font77 <vimalkg@gmail.com>
2020-02-18 09:12:59 +0530

    voes times romn

---
+++ hom_viml_7f/cpp_phont24/phont_test.cpp
@@ -0,0 +1,78 @@
+/*
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+abcdefghijklmnopqrstuvwxyz
+
+daktr ki wvai bzux hi krrvi xhi.
+qNgutha wikhane ke kam axa h.
+qNguliya ginne ke kam aaxi h.
+perr ki dali se qNgur ki bel ltki hui h.
+aNgur kNgaru bNgali
+X yh glax h.kl
+sin30=
+zeksadesiml nmbrs
+----------------
+0 1 2 3 4 5 6 7 8 9 A B C D E F
+----------------
+
+q a i u e o
+k kh g gh
+c ch j jh
+t th d dh
+w wh x xh n
+p ph b bh m
+y r rr l ll
+s sh v z
+
+k qk ka ki kii ku kuu ke kqi ko kqu kN
+kh qkh kha khi khii khu khuu khe khqi kho khqu khN
+
+g qg ga gi gii gu guu ge gqi go gqu gN
+gh qgh gha ghi ghii ghu ghuu ghe ghqi gho ghqu ghN
+
+c qc ca ci cii cu cuu ce cqi co cqu cN
+ch qch cha chi chii chu chuu che chqi cho chqu chN
+
+j qj ja ji jii ju juu je jqi jo jqu jN
+jh qjh jha jhi jhii jhu jhuu jhe jhqi jho jhqu jhN
+
+t qt ta ti tii tu tuu te tqi to tqu tN
+th qth tha thi thii thu thuu the thqi tho thqu thN
+
+d qd da di dii du duu de dqi do dqu dN
+dh qdh dha dhi dhii dhu dhuu dhe dhqi dho dhqu dhN
+
+w qw wa wi wii wu wuu we wqi wo wqu wN
+wh qwh wha whi whii whu whuu whe whqi who whqu whN
+
+x qx xa xi xii xu xuu xe xqi xo xqu xN
+xh qxh xha xhi xhii xhu xhuu xhe xhqi xho xhqu xhN
+
+n qn na ni nii nu nuu ne nqi no nqu Nn
+
+p qp pa pi pii pu puu pe pqi po pqu pN
+ph qph pha phi phii phu phuu phe phqi pho phqu phN
+
+b qb ba bi bii bu buu be bqi bo bqu bN
+bh qbh bha bhi bhii bhu bhuu bhe bhqi bho bhqu bhN
+
+m qm ma mi mii mu muu me mqi mo mqu mN
+
+y qy ya yi yii yu yuu ye yqi yo yqu yN
+
+r qr ra ri rii ru ruu re rqi ro rqu rN
+rr qrr rra rri rrii rru rruu rre rrqi rro rrqu rrN
+
+l ql la li lii lu luu le lqi lo lqu lN
+ll qll lla lli llii llu lluu lle llqi llo llqu llN
+
+v qv va vi vii vu vuu ve vqi vo vqu vN
+
+s qs sa si sii su suu se sqi so squ sN
+sh qsh sha shi shii shu shuu she shqi sho shqu shN
+
+z qz za zi zii zu zuu ze zqi zo zqu zN
+*/
+if(aHb)
+{
+    
+}
---
+++ hom_viml_7f/del/cpp_phont24/phont_test.cpp
@@ -0,0 +1,66 @@
+ABCDEFGHI JKLMNOPQRSTUV WXYZ
+abcdefghijklmnopqrstuvwxyz
+  
+
+zeksadesiml nmbrs
+----------------
+0 1 2 3 4 5 6 7 8 9 A B C D E F
+----------------
+
+q a i u e o
+k kh g gh
+c ch j jh
+t th d dh
+w wh x xh n
+p ph b bh m
+y r rr l ll
+s sh v z
+
+k qk ka ki kii ku kuu ke kqi ko kqu knn
+kh qkh kha khi khii khu khuu khe khqi kho khqu khnn
+
+g qg ga gi gii gu guu ge gqi go gqu gnn
+gh qgh gha ghi ghii ghu ghuu ghe ghqi gho ghqu ghnn
+
+c qc ca ci cii cu cuu ce cqi co cqu cnn
+ch qch cha chi chii chu chuu che chqi cho chqu chnn
+
+j qj ja ji jii ju juu je jqi jo jqu jnn
+jh qjh jha jhi jhii jhu jhuu jhe jhqi jho jhqu jhnn
+
+t qt ta ti tii tu tuu te tqi to tqu tnn
+th qth tha thi thii thu thuu the thqi tho thqu thnn
+
+d qd da di dii du duu de dqi do dqu dnn
+dh qdh dha dhi dhii dhu dhuu dhe dhqi dho dhqu dhnn
+
+w qw wa wi wii wu wuu we wqi wo wqu wnn
+wh qwh wha whi whii whu whuu whe whqi who whqu whnn
+
+x qx xa xi xii xu xuu xe xqi xo xqu xnn
+xh qxh xha xhi xhii xhu xhuu xhe xhqi xho xhqu xhnn
+
+n qn na ni nii nu nuu ne nqi no nqu nnn
+
+p qp pa pi pii pu puu pe pqi po pqu pnn
+ph qph pha phi phii phu phuu phe phqi pho phqu phnn
+
+b qb ba bi bii bu buu be bqi bo bqu bnn
+bh qbh bha bhi bhii bhu bhuu bhe bhqi bho bhqu bhnn
+
+m qm ma mi mii mu muu me mqi mo mqu mnn
+
+y qy ya yi yii yu yuu ye yqi yo yqu ynn
+
+r qr ra ri rii ru ruu re rqi ro rqu rnn
+rr qrr rra rri rrii rru rruu rre rrqi rro rrqu rrnn
+
+l ql la li lii lu luu le lqi lo lqu lnn
+ll qll lla lli llii llu lluu lle llqi llo llqu llnn
+
+v qv va vi vii vu vuu ve vqi vo vqu vnn
+
+s qs sa si sii su suu se sqi so squ snn
+sh qsh sha shi shii shu shuu she shqi sho shqu shnn
+
+z qz za zi zii zu zuu ze zqi zo zqu znn
