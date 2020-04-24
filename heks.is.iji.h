8ca9403d7656e23fa7d13d8392ee71e05d792a4b
Font77 <vimalkg@gmail.com>
2020-04-24 16:57:31 +0530

    Adid heksbin dir

---
+++ heks.is.iji.h
@@ -0,0 +1,6 @@
+heksadesiml : √10=4  ,  √40=8 ,  √64=A=ten ,  √100=10 ,  √400=20 , √4000=80
+desiml : √10=oh mai god??? , √40=oh mai god??? , √100=10 , √400=20 , √4000=oh mai god??? , 
+vich mAThs is iji for kids kmpyutrs mni Atstra ?
+Thmb tu sho . fiNgArs tu kaunt .
+
+koNgrAts rajesh on startiNg At insaId!
