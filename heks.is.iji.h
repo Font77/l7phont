2990432ca8a1852ddd122275c1166a2da67a16c6
Font77 <vimalkg@gmail.com>
2020-05-01 15:46:52 +0530

    modiphaid x(T)

--- heks.is.iji.h
+++ heks.is.iji.h
@@ -4,3 +4,14 @@ vich mAThs is iji for kids kmpyutrs mni Atstra ?
 Thmb tu sho . fiNgArs tu kaunt .
 
 koNgrAts rajesh on startiNg At insaId!
+----------
+heksadesiml Aur bainAri me 1 se vnti(10) Tk kauntiNg (ginTi)
+vidio: https://youtu.be/6roYhETLQAU
+roboto heks roboto bin fonts
+1vn) https://github.com/Font77/7f/blob/master/heksfonts/roboto_heks.ttf
+2tu) https://github.com/Font77/7f/blob/master/binheksi/roboto_bin.ttf
+----------
+vindoj kibord viDh 8+8=vnti=10 kiij
+https://www.youtube.com/watch?v=buxokmE1yIw
+https://github.com/Font77/vindoj_kibord_vach_ring
+----------
