96985659ec74ab11ec7cea1e59db13d91610ab7a
Font77 <vimalkg@gmail.com>
2020-05-06 15:01:23 +0530

    vk.com/etphor facebook.com/heksoin

--- heks.is.iji.h
+++ heks.is.iji.h
@@ -6,12 +6,15 @@ Thmb tu sho . fiNgArs tu kaunt .
 koNgrAts rajesh on startiNg At insaId!
 ----------
 heksadesiml Aur bainAri me 1 se vnti(10) Tk kauntiNg (ginTi)
-vidio: https://youtu.be/6roYhETLQAU
+youtu.be/6roYhETLQAU
+youtu.be/cbe5NUdIdrM
 roboto heks roboto bin fonts
-1vn) https://github.com/Font77/7f/blob/master/heksfonts/roboto_heks.ttf
-2tu) https://github.com/Font77/7f/blob/master/binheksi/roboto_bin.ttf
+1vn) github.com/Font77/7f/blob/master/heksfonts
+2tu) github.com/Font77/7f/blob/master/binheksi
+2tu) github.com/Font77/7f/blob/master/binheksi/roboto_bin.ttf
 ----------
 vindoj kibord viDh 8+8=vnti=10 kiij
-https://www.youtube.com/watch?v=buxokmE1yIw
-https://github.com/Font77/vindoj_kibord_vach_ring
+youtube.com/watch?v=buxokmE1yIw
+github.com/Font77/vindoj_kibord_vach_ring
 ----------
+ math maths mathematics ascii table font fonts custom fonts fpga computerscience bitcoin crypto webfont bilingual office suit ms office elearning online education embedded
